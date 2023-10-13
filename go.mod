b4e0e26a2d845f457a6bd5a9ffefd56845910625
Taylor Monacelli <taylormonacelli@gmail.com>
2023-10-13 07:52:29 -0700

    working

--- go.mod
+++ go.mod
@@ -2,4 +2,3 @@ module github.com/taylormonacelli/bluecare
 
 go 1.21.1
 
-require gopkg.in/yaml.v2 v2.4.0
--- main.go
+++ main.go
@@ -4,42 +4,46 @@ import (
 	"encoding/json"
 	"fmt"
 	"io"
+	"log/slog"
 	"net/http"
 	"os"
-
-	"gopkg.in/yaml.v2"
 )
 
 func main() {
 	url := "https://raw.githubusercontent.com/aws/aws-sdk-net/master/sdk/src/Core/endpoints.json"
-	localJSONPath := "endpoints.json"
-	localYAMLPath := "endpoints.yaml"
+	jsonIncoming := "endpoints.json"
+	jsonEdited := "endpoints_edited.json"
 
-	if _, err := os.Stat(localJSONPath); os.IsNotExist(err) {
+	existingData := make(map[string]map[string]string)
+	if _, err := os.Stat(jsonEdited); err == nil {
+		existingData = readExistingData(jsonEdited)
+	}
+
+	if _, err := os.Stat(jsonIncoming); os.IsNotExist(err) {
 		response, err := http.Get(url)
 		if err != nil {
-			fmt.Println("Failed to download the JSON file.")
+			slog.Error("Failed to download the JSON file.")
 			os.Exit(1)
 		}
 		defer response.Body.Close()
 
-		file, err := os.Create(localJSONPath)
+		file, err := os.Create(jsonIncoming)
 		if err != nil {
-			fmt.Println("Failed to create JSON file.")
+			slog.Error("Failed to create JSON file.")
 			os.Exit(1)
 		}
 		defer file.Close()
 
 		_, err = io.Copy(file, response.Body)
 		if err != nil {
-			fmt.Println("Failed to save JSON file.")
+			slog.Error("Failed to save JSON file.")
 			os.Exit(1)
 		}
 	}
 
-	jsonFile, err := os.Open(localJSONPath)
+	jsonFile, err := os.Open(jsonIncoming)
 	if err != nil {
-		fmt.Println("Failed to read JSON file.")
+		slog.Error("Failed to read JSON file.")
 		os.Exit(1)
 	}
 	defer jsonFile.Close()
@@ -48,74 +52,82 @@ func main() {
 	var data map[string]interface{}
 	err = decoder.Decode(&data)
 	if err != nil {
-		fmt.Println("Failed to parse JSON data.")
+		slog.Error("Failed to parse JSON data.")
 		os.Exit(1)
 	}
 
 	partitions, ok := data["partitions"].([]interface{})
 	if !ok || len(partitions) == 0 {
-		fmt.Println("Invalid JSON data structure.")
+		slog.Error("Invalid JSON data structure.")
 		os.Exit(1)
 	}
 
 	partition, ok := partitions[0].(map[string]interface{})
 	if !ok {
-		fmt.Println("Invalid JSON data structure.")
+		slog.Error("Invalid JSON data structure.")
 		os.Exit(1)
 	}
 
 	services, ok := partition["services"].(map[string]interface{})
 	if !ok {
-		fmt.Println("Invalid JSON data structure.")
+		slog.Error("Invalid JSON data structure.")
 		os.Exit(1)
 	}
 
-	var newServiceNames []string
+	newServiceNames := make(map[string]map[string]string)
 	for serviceName := range services {
-		newServiceNames = append(newServiceNames, serviceName)
-	}
-
-	yamlData := make(map[string]interface{})
-	yamlData["service_names"] = newServiceNames
-
-	if _, err := os.Stat(localYAMLPath); os.IsNotExist(err) {
-		err = writeYAMLFile(localYAMLPath, yamlData)
-		if err != nil {
-			fmt.Println("Failed to write YAML file.")
-			os.Exit(1)
+		if existingURL, exists := existingData[serviceName]; exists {
+			newServiceNames[serviceName] = existingURL
+		} else {
+			newServiceNames[serviceName] = map[string]string{
+				"console": fmt.Sprintf("https://us-west-1.console.aws.amazon.com/%s/home?region=us-west-1#", serviceName),
+			}
 		}
 	}
 
-	fmt.Printf("New service names added: %v\n", newServiceNames)
-	fmt.Println("Merged Data:")
-	printYAML(yamlData)
-}
-
-func writeYAMLFile(filePath string, data interface{}) error {
-	yamlBytes, err := yaml.Marshal(data)
-	if err != nil {
-		return err
+	jsonData := map[string]interface{}{
+		"service_names": newServiceNames,
 	}
 
-	yamlFile, err := os.Create(filePath)
-	if err != nil {
-		return err
-	}
-	defer yamlFile.Close()
-
-	_, err = yamlFile.Write(yamlBytes)
-	if err != nil {
-		return err
-	}
-
-	return nil
-}
-
-func printYAML(data interface{}) {
-	yamlBytes, err := yaml.Marshal(data)
-	if err != nil {
-		fmt.Println("Failed to marshal data to YAML.")
+	if err := writeJSONFile(jsonEdited, jsonData); err != nil {
+		slog.Error("Failed to write JSON file.")
 		os.Exit(1)
 	}
-	fmt.Printf("%s\n", yamlBytes)
+
+	fmt.Printf("Service names and URLs updated:\n")
+}
+
+func writeJSONFile(filePath string, data interface{}) error {
+	jsonBytes, err := json.MarshalIndent(data, "", "  ") // Indent with two spaces for pretty printing
+	if err != nil {
+		return err
+	}
+
+	jsonFile, err := os.Create(filePath)
+	if err != nil {
+		return err
+	}
+	defer jsonFile.Close()
+
+	_, err = jsonFile.Write(jsonBytes)
+	return err
+}
+
+func readExistingData(filePath string) map[string]map[string]string {
+	jsonFile, err := os.Open(filePath)
+	if err != nil {
+		slog.Error("Failed to read existing JSON file.")
+		return make(map[string]map[string]string)
+	}
+	defer jsonFile.Close()
+
+	decoder := json.NewDecoder(jsonFile)
+	var data map[string]map[string]map[string]string
+	err = decoder.Decode(&data)
+	if err != nil {
+		slog.Error("Failed to parse existing JSON data.")
+		return make(map[string]map[string]string)
+	}
+
+	return data["service_names"]
 }
